ae7edd054ab5a09e630c6f1ed33ffd0297298d3d
idooru <shere1765@gmail.com>
2023-06-21 18:02:04 +0900

    HTTP 요청 데이터 - POST HTML Form

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -1,10 +1,12 @@
 package hello.servlet.basic.request;
 
+import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
 
@@ -14,9 +16,7 @@ public class RequestParamServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("[전체 파라미터 조회] - start");
-        request.getParameterNames().asIterator().forEachRemaining(paramName ->
-                System.out.printf("%s = %s%n", paramName, request.getParameter(paramName))
-        );
+        request.getParameterNames().asIterator().forEachRemaining(paramName -> System.out.printf("%s = %s%n", paramName, request.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회] - end");
         System.out.println();
 
@@ -34,6 +34,53 @@ public class RequestParamServlet extends HttpServlet {
             System.out.println("username = " + name);
         }
 
-        response.getWriter().write("ok");
+        Parameter parameter = new Parameter(username, age);
+        Response<Parameter> responseResult = new ResponseBuilder<Parameter>().setSuccess(true).setMessage("provide parameter info").setResult(parameter).build();
+
+
+        final Gson gson = new Gson();
+        final String toJson = gson.toJson(responseResult);
+
+        response.getWriter().write(toJson);
     }
+
+    @RequiredArgsConstructor
+    static class Parameter {
+        private final String username;
+        private final String age;
+    }
+
+    static class ResponseBuilder<T> {
+        private boolean success;
+        private String message;
+        private T result;
+
+
+        public ResponseBuilder<T> setSuccess(boolean success) {
+            this.success = success;
+            return this;
+        }
+
+        public ResponseBuilder<T> setMessage(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public ResponseBuilder<T> setResult(T result) {
+            this.result = result;
+            return this;
+        }
+
+        public Response<T> build() {
+            return new Response<>(success, message, result);
+        }
+    }
+
+    @RequiredArgsConstructor
+    static class Response<T> {
+        private final boolean success;
+        private final String message;
+        private final T result;
+    }
+
 }
