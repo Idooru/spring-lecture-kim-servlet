ce5592ae0c134c41d3a5880cc2ef285a1db4cc41
Idooru <shere1765@gmail.com>
2023-07-27 19:45:31 +0900

    HttpServletResponse - 기본 사용법

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,55 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // [status-line]
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+        // [response-headers]
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+
+        // [Header 편의 메서드]
+        content(response);
+
+        cookie(response);
+
+        redirect(response);
+
+        PrintWriter writer = response.getWriter();
+        writer.println("안녕하세요.");
+    }
+
+    private void content(HttpServletResponse response) {
+//        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+    }
+
+    private void cookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(30);
+        cookie.setSecure(true);
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_FOUND);
+        response.setHeader("Location", "/basic/hello-form.html");
+    }
+}
